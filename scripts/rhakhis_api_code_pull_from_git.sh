a318cef8c2aea9e96760f4366a97cc1585ca858d
Roger Hyam <rhyam@rbge.org.uk>
2023-07-30 08:09:19 +0100

    pull script

---
+++ scripts/rhakhis_api_code_pull_from_git.sh
@@ -0,0 +1,6 @@
+# This script should be copied to /var/wfo-list and run from there
+# It will be used when it, and the rest of the code, doesn't exist.
+# it also needs to be in the same place as the other sync_from and sync_to scripts
+cd /var/wfo-list/rhakhis/api
+git stash
+git pull
