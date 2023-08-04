4f5fcca035e53b4d40e198fcba31fa5c7290f26e
Roger Hyam <rhyam@rbge.org.uk>
2023-08-04 09:05:35 +0100

    start work

---
+++ scripts/pflanzenreich_linking.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+
+https://www.biodiversitylibrary.org/api3?op=GetTitleMetadata&format=json&items=true&id=250&apikey=a3d3fd5f-e612-41b5-b72c-e4f36bcad8f7
+
+
+get part metadata
+https://www.biodiversitylibrary.org/api3?op=GetPartMetadata&format=json&items=true&id=68126&apikey=a3d3fd5f-e612-41b5-b72c-e4f36bcad8f7
+
+
+*/
+
+$api_key = "a3d3fd5f-e612-41b5-b72c-e4f36bcad8f7";
+$title_id = 250;
+
+// get all the hefts and their data
+
