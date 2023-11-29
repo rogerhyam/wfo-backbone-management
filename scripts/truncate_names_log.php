dcefd9207a64eab34fc3a1701313c1cf243dd1a0
Roger Hyam <rhyam@rbge.org.uk>
2023-11-29 14:39:55 +0000

    truncates now

--- scripts/truncate_names_log.php
+++ scripts/truncate_names_log.php
@@ -6,13 +6,14 @@ require_once('../config.php');
 // this script will delete every record over 6 months old
 // records older than 6 months can be recovered from the 
 // solstice dumps if that should ever be necessary.
-
+// not a long script!
 /*
 
 ALTER TABLE `promethius`.`names_log` 
 ADD INDEX `modified` USING BTREE (`modified`) VISIBLE;
 
 */
-
-
-$sql = "DELETE FROM names_log ";
+echo "\nRemoving name logs over 6 months old.\n";
+$mysqli->query("DELETE FROM names_log where modified < now() - interval 6 month;");
+echo number_format($mysqli->affected_rows, 0) . " rows removed.\n";
+echo "Done.\n";
