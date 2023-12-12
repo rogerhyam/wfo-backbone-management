ed4d60eb9b298427c7d61480d9ce864819dc1544
Roger Hyam <r.hyam@rbge.org.uk>
2023-12-12 15:15:04 +0000

    new cleanup script

---
+++ scripts/references_remove_escaped_chars.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('../config.php');
+
+$sql = "SELECT *
+    FROM promethius.references 
+    WHERE kind = 'literature' 
+    AND display_text LIKE '%&amp;%' 
+    OR display_text LIKE '%&lt;%'
+    OR display_text LIKE '%<scp>%'";
+
+$response = $mysqli->query($sql);
+$rows = $response->fetch_all(MYSQLI_ASSOC);
+
+foreach($rows as $row){
+    $id = $row['id'];
+    $old = $row['display_text'];
+    $new = html_entity_decode($old);
+    $new = strip_tags($new);
+    $new_safe = $mysqli->real_escape_string($new);
+    $mysqli->query("UPDATE `references` SET display_text = '$new_safe' WHERE id = $id ");
+    echo "$id\n\t$old\n\t$new\n";
+    if($mysqli->error){
+        echo $mysqli->error;
+        exit;
+    }
+}
