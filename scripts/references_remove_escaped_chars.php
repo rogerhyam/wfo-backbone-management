f8105746f5400248829e2860ae70a20563fd69cf
Roger Hyam <r.hyam@rbge.org.uk>
2023-12-20 11:44:42 +0000

    apostrophy removal

--- scripts/references_remove_escaped_chars.php
+++ scripts/references_remove_escaped_chars.php
@@ -2,23 +2,27 @@
 
 require_once('../config.php');
 
-$sql = "SELECT *
+echo "\nRemoving escaped characters from references.\n";
+
+    $sql = "SELECT *
     FROM promethius.references 
     WHERE kind = 'literature' 
-    AND display_text LIKE '%&amp;%' 
-    OR display_text LIKE '%&lt;%'
-    OR display_text LIKE '%<%'";
+    AND REGEXP_LIKE(display_text, '&[^ ]+;')
+    OR display_text LIKE '%<%';";
 
 $response = $mysqli->query($sql);
 $rows = $response->fetch_all(MYSQLI_ASSOC);
 
+echo "\t ". count($rows) . " references selected.\n";
 
 $count = 0;
 
 foreach($rows as $row){
     $id = $row['id'];
     $old = $row['display_text'];
-    $new = html_entity_decode($old);
+    // weird error 
+    $new = str_replace('&Apos;', '&apos;', $old);
+    $new = html_entity_decode($new, ENT_QUOTES | ENT_XML1, 'UTF-8');
     $new = strip_tags($new);
     $new_safe = $mysqli->real_escape_string($new);
     $mysqli->query("UPDATE `references` SET display_text = '$new_safe' WHERE id = $id ");
