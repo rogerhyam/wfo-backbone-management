a6ac0c957f31e125069e68749518f7cbda654687
Roger Hyam <rhyam@rbge.org.uk>
2023-12-29 08:44:09 +0000

    tweaks during release

--- sql/remove_personal_data.sql
+++ sql/remove_personal_data.sql
@@ -5,8 +5,3 @@ Remove the personal data (access keys) from the database ready for general relea
 SET SQL_SAFE_UPDATES = 0;
 update users set wfo_access_token = null, orcid_access_token = null, orcid_refresh_token = null, orcid_expires_in = null, orcid_raw = null;
 SET SQL_SAFE_UPDATES = 1;
-
-/* 
-Drop botalista as it is in the last versions and never changes and will eventually go
-*/
-drop table botalista_dump_2;
